b5f411253620f11d22de0d886c840395a8e3b591
Alejandra Montano Romero <amontan6@WSE-Gray-J20RYQTCXF.local>
2025-11-03 18:36:46 -0500

    Full script that has the PyMOL observer, packing and minimization

--- pilot/apps/amontan6/bootcamp_app.py
+++ pilot/apps/amontan6/bootcamp_app.py
@@ -3,6 +3,11 @@ import argparse
 from pyrosetta import *
 from pyrosetta.rosetta import numeric
 from pyrosetta.rosetta.protocols.moves import MonteCarlo
+from pyrosetta.rosetta.core.kinematics import MoveMap
+from pyrosetta.rosetta.core.optimization import MinimizerOptions, AtomTreeMinimizer
+from pyrosetta.rosetta.core.pack.task import TaskFactory
+from pyrosetta.rosetta.core.pack import pack_rotamers
+
 
 init(extra_options="-ignore_unrecognized_res")
 
@@ -18,11 +23,26 @@ mypose = pose_from_pdb(args.filename)
 print(f"Loaded pose with {mypose.total_residue()} residues from: {args.filename}")
 
 scorefxn = get_score_function(True)
-score = scorefxn(mypose)
-print(score)
+current_score = scorefxn(mypose)
+print(current_score)
+print("-------------")
 
 temperature = 1.0
-mc = MonteCarlo(mypose, scorefxn, temperature) ##come back and check this sytax
+mc = MonteCarlo(mypose, scorefxn, temperature) ##come back and check this syntax
+
+# Set up MoveMap for backbone and sidechain movement
+movemap = MoveMap()
+movemap.set_bb(True)
+movemap.set_chi(True)
+
+# Minimizer setup
+min_opts = MinimizerOptions("lbfgs_armijo_atol", 0.01, True)
+minimizer = AtomTreeMinimizer()
+
+minimizer.run(mypose, movemap, scorefxn, min_opts)
+
+the_observer = protocols.moves.PyMOLObserver(mypose, True, 0)
+the_observer.pymol().apply(mypose)
 
 for i in range(1):
     n_res = mypose.total_residue()
@@ -36,6 +56,18 @@ for i in range(1):
     mypose.set_phi(residue_index, orig_phi + phi_perturb)
     mypose.set_psi(residue_index, orig_psi + psi_perturb)
 
+    tf = TaskFactory()
+    task = tf.create_task_and_apply_taskoperations(mypose)
+    task.restrict_to_repacking()
+    pack_rotamers(mypose, scorefxn, task)
+
     accepted = mc.boltzmann(mypose)
-    print(accepted)
+    #print(accepted)
+
+mc.recover_low(mypose)            # overwrite pose with best-so-far
+mypose.dump_pdb("out_best.pdb")
+
+print(scorefxn(mypose))
+
+
 
