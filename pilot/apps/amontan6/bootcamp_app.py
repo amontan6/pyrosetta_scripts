075b1922bee7945625e5d2bac3440d9e28d8021c
Alejandra Montano Romero <amontan6@WSE-Gray-J20RYQTCXF.local>
2025-11-03 17:07:24 -0500

    Additions where begins a loop to perturb torsion angles of a random residue and passes it though MonteCarlo boltzmann

--- pilot/apps/amontan6/bootcamp_app.py
+++ pilot/apps/amontan6/bootcamp_app.py
@@ -1,6 +1,8 @@
 import sys
 import argparse
 from pyrosetta import *
+from pyrosetta.rosetta import numeric
+from pyrosetta.rosetta.protocols.moves import MonteCarlo
 
 init(extra_options="-ignore_unrecognized_res")
 
@@ -18,3 +20,22 @@ print(f"Loaded pose with {mypose.total_residue()} residues from: {args.filename}
 scorefxn = get_score_function(True)
 score = scorefxn(mypose)
 print(score)
+
+temperature = 1.0
+mc = MonteCarlo(mypose, scorefxn, temperature) ##come back and check this sytax
+
+for i in range(1):
+    n_res = mypose.total_residue()
+    random_res = numeric.random.uniform()
+    residue_index = int(random_res * n_res) + 1
+
+    phi_perturb = numeric.random.gaussian()
+    psi_perturb = numeric.random.gaussian()
+    orig_phi = mypose.phi(residue_index)
+    orig_psi = mypose.psi(residue_index)
+    mypose.set_phi(residue_index, orig_phi + phi_perturb)
+    mypose.set_psi(residue_index, orig_psi + psi_perturb)
+
+    accepted = mc.boltzmann(mypose)
+    print(accepted)
+
