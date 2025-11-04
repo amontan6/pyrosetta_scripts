ae3e7e7a580fa9ae278e7318de2341b46b8f8255
Alejandra Montano Romero <amontan6@WSE-Gray-J20RYQTCXF.local>
2025-11-04 11:44:15 -0500

    Test cases for all instances of enqueue, dequeue, size, and is_empty

--- pilot/apps/amontan6/test_queue.py
+++ pilot/apps/amontan6/test_queue.py
@@ -1,3 +1,56 @@
+from queue import Queue
+
 def test_first():
     print("Running my first unit tests!")
     assert True
+
+def test_enqueue(): #checking about adding an item to the end of the queue
+    imaginary = Queue()
+    for i in range(len(imaginary)):
+        if len(imaginary) - 1 == imaginary.enqueue():
+            assert True
+
+def test_enqueue2(): #checking about adding an item to the end of the queue
+    imaginary = Queue()
+    for i in range(len(imaginary)):
+        if imaginary[0] == imaginary.enqueue():
+            assert False
+
+def test_dequeue():
+    imaginary = Queue()
+    for i in range(len(imaginary)):
+        first_index = imaginary.pop(0) #pop removes and returns!!
+        if first_index == imaginary.dequeue():
+            assert True
+
+def test_dequeue2():
+    imaginary = Queue()
+    for i in range(len(imaginary)):
+        first_index = imaginary.pop(-1)
+        if first_index == imaginary.dequeue():
+            assert False
+
+def test_size():
+    imaginary = Queue()
+    for i in range(len(imaginary)):
+        if len(imaginary) == imaginary.size():
+            assert True
+
+def test_size2():
+    imaginary = Queue()
+    for i in range(len(imaginary)):
+        if len(imaginary) != imaginary.size():
+            assert False
+
+def test_isempty():
+    imaginary = Queue()
+    for i in range(len(imaginary)):
+        if len(imaginary) == 0:
+            assert True
+
+def test_isempty2():
+    imaginary = Queue()
+    for i in range(len(imaginary)):
+        if len(imaginary) != 0:
+            assert False
+
